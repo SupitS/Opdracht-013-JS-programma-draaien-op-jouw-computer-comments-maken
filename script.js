862c19234de9ef2cb99e2a79e6356e1b82907990
SupitS <84420067+SupitS@users.noreply.github.com>
2021-06-05 16:47:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Opdracht 013: JS programma draaien op jouw computer + comments maken
+
+console.log('Hallo Winc Academy studenten');
+
+// Dit is een grote som 
+/*let number = 1230941 * 1823794;
+console.log(number);
+
+let number2 = 637263 / 54;
+console.log(number2); */
+
+
